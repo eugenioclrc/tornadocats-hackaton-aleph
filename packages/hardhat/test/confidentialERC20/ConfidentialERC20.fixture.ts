88ec88deb076b4cf3777305d5c0795d208ec8f69
eugenioclrc <eugenioclrc@gmail.com>
2024-12-14 14:05:42 -0300

    extra test

--- packages/hardhat/test/confidentialERC20/ConfidentialERC20.fixture.ts
+++ packages/hardhat/test/confidentialERC20/ConfidentialERC20.fixture.ts
@@ -6,8 +6,6 @@ import { getSigners } from "../signers";
 export async function deployConfidentialERC20Fixture(addressToken): Promise<WrappedPrivacyERC20> {
   const signers = await getSigners();
   
-
-
   const contractFactory = await ethers.getContractFactory("WrappedPrivacyERC20");
   const contract = await contractFactory.connect(signers.alice).deploy(addressToken);
   await contract.waitForDeployment();
