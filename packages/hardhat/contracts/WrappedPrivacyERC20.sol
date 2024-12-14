40512bd2c3a59296def2866ad623baab67371b86
eugenioclrc <eugenioclrc@gmail.com>
2024-12-14 13:00:53 -0300

    Fix to issues

--- packages/hardhat/contracts/WrappedPrivacyERC20.sol
+++ packages/hardhat/contracts/WrappedPrivacyERC20.sol
@@ -3,15 +3,19 @@
 pragma solidity ^0.8.24;
 
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import {SafeERC20} from "@openzeppelin/contracts/utils/SafeERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {ConfidentialERC20} from "fhevm-contracts/contracts/token/ERC20/ConfidentialERC20.sol";
+
+// import types from Zama's FHE library
+import "fhevm/lib/TFHE.sol";
 
 /// @notice This contract implements an encrypted ERC20-like token with confidential balances using Zama's FHE library.
 /// @dev It supports typical ERC20 functionality such as transferring tokens, minting, and setting allowances,
 /// @dev but uses encrypted data types.
-contract WrappedPrivacyERC20 is ConfidentialERC20 {
+contract WrappedPrivacyERC20 is ConfidentialERC20{
     using SafeERC20 for IERC20;
     using SafeCast for uint256;
 
@@ -34,13 +38,24 @@ contract WrappedPrivacyERC20 is ConfidentialERC20 {
             amountInPrivacyToken = amount / (10 ** (UNDERLYING_DECIMALS - decimals()));
             amountInUnderlyingToken = amountInPrivacyToken * (10 ** (UNDERLYING_DECIMALS - decimals()));
         } else if (UNDERLYING_DECIMALS < decimals()) {
-            amountInPrivacyToken = amont * 10 ** (decimals() - UNDERLYING_DECIMALS);
+            amountInPrivacyToken = amount * 10 ** (decimals() - UNDERLYING_DECIMALS);
             amountInUnderlyingToken = amount;
         }
     }
 
+    /**
+     * @dev It does not incorporate any overflow check. It must be implemented
+     *      by the function calling it.
+     */
+     function _unsafeBurnNoEvent(address account, uint64 amount) internal virtual {
+        euint64 newBalanceAccount = TFHE.sub(_balances[account], amount);
+        _balances[account] = newBalanceAccount;
+        TFHE.allowThis(newBalanceAccount);
+        TFHE.allow(newBalanceAccount, account);
+    }
+
     // @notice Mints `amount` of tokens to the caller. No encryption is used since amount transferred is public.
-    function mint(uint256 amount) {
+    function mint(uint256 amount) external {
         (uint256 amountInPrivacyToken, uint256 amountInUnderlyingToken) = _normalizeAmount(amount);
         
         // @dev transfer underlying token to this contract, with a secure balance check
@@ -56,11 +71,11 @@ contract WrappedPrivacyERC20 is ConfidentialERC20 {
     }
 
     // @notice Burns `amount` of tokens from the caller.
-    function burn(uint256 amount) {
+    function burn(uint256 amount) external {
         (uint256 amountInPrivacyToken, uint256 amountInUnderlyingToken) = _normalizeAmount(amount);
 
         // @dev burn the same amount of privacy token from the caller
-        _unsafeBurn(msg.sender, amountInPrivacyToken.toUint64());
+        _unsafeBurnNoEvent(msg.sender, amountInPrivacyToken.toUint64());
 
         // @dev transfer underlying token to the caller
         IERC20(UNDERLYING).safeTransfer(msg.sender, amountInUnderlyingToken);
--- packages/hardhat/contracts/mocks/MockToken.sol
+++ packages/hardhat/contracts/mocks/MockToken.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals, uint256 supply) ERC20(name, symbol, decimals) {
+    uint8 private _decimals;
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+    constructor(string memory name, string memory symbol, uint8 decimals_, uint256 supply) ERC20(name, symbol) {
+        _decimals = decimals_;
         _mint(msg.sender, supply);
     }
 }
